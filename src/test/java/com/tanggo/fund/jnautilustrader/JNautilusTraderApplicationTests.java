871dceb3c6f2518ebc2f84c66ca461bc13ed1b61
thy3368 <thy_email@126.com>
2026-01-14 11:27:44 +0800

    分析量化的领域模型

---
+++ src/test/java/com/tanggo/fund/jnautilustrader/JNautilusTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanggo.fund.jnautilustrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JNautilusTraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
